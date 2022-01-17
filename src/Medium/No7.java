733688742cff145cbbb49183eecf21c1c9cb933e
罗延瀚 <luoyanhan@alphaleader.com>
2022-01-17 15:04:03 +0800

    feat(medium):7

---
+++ src/Medium/No7.java
@@ -0,0 +1,18 @@
+package Medium;
+
+public class No7 {
+    class Solution {
+        public int reverse(int x) {
+            int res = 0;
+            while (x!=0){
+                if (res<Integer.MIN_VALUE/10 || res>Integer.MAX_VALUE/10){
+                    return 0;
+                }
+                int num = x % 10;
+                x /= 10;
+                res = res*10 + num;
+            }
+            return res;
+        }
+    }
+}
