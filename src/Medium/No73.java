8358b1ec88b1a89b90a12bddbeb1885ec38e248b
罗延瀚 <luoyanhan@alphaleader.com>
2022-03-08 16:57:14 +0800

    feat(medium):add 73

---
+++ src/Medium/No73.java
@@ -0,0 +1,34 @@
+package Medium;
+
+public class No73 {
+    class Solution {
+        public void setZeroes(int[][] matrix) {
+            int m = matrix.length;
+            int n = matrix[0].length;
+            int[] rows = new int[m];
+            int[] cols = new int[n];
+            for (int i=0; i<m; i++){
+                for (int j=0; j<n; j++){
+                    if (matrix[i][j]==0){
+                        rows[i] = 1;
+                        cols[j] = 1;
+                    }
+                }
+            }
+            for (int i=0; i<m; i++){
+                if (rows[i]==1){
+                    for (int j=0; j<n; j++){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+            for (int j=0; j<n; j++){
+                if (cols[j]==1){
+                    for (int i=0; i<m; i++){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
