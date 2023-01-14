de76d08e610530b00c655921e8749d8fda68c1c1
luoyanhan <707025023@qq.com>
2023-01-14 14:09:10 +0800

    feat(mediuum): update 49

---
+++ src/Medium/NO49/Solution.java
@@ -0,0 +1,18 @@
+package Medium.NO49;
+
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for (String str:strs){
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
