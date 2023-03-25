2db8cf850f8f6199a0ef3e79d968bae6b3624121
luoyanhan <707025023@qq.com>
2023-03-25 11:32:37 +0800

    feat(mediuum): add 201

---
+++ src/Medium/NO201/Solution.java
@@ -0,0 +1,13 @@
+package Medium.NO201;
+
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        while (left<right){
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+        return left << shift;
+    }
+}
